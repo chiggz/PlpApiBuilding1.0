ea285edd087789bd721ed1e2b830e0736bf6ac87
chiggzwilly <33693984-chiggzwilly@users.noreply.replit.com>
2024-04-27 20:39:18 +0000

    Initial commit

---
+++ django_project/views.py
@@ -0,0 +1,16 @@
+import requests
+from django.shortcuts import render
+
+
+def home(request):
+  #USING APIS.
+  response = requests.get('https://api.github.com/events')
+  data = response.json()
+  result = data[1]["id"]
+
+  # Example 2
+  reponse2 = requests.get('https://dog.ceo/api/breeds/image/random')
+  data2 = reponse2.json()
+  result2 = data2["message"]
+
+  return render(request, 'templates/index.html', {'result': result, 'result2': result2},)
