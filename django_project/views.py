8d5984e02c68ed85e192e75e55fbf64e68105ea5
William Ngarachu <ngarachuwilliam@gmail.com>
2024-04-27 23:12:44 +0000

    Added the cat feature & Styling

--- django_project/views.py
+++ django_project/views.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from django.shortcuts import render
 
 
@@ -12,5 +13,15 @@ def home(request):
   reponse2 = requests.get('https://dog.ceo/api/breeds/image/random')
   data2 = reponse2.json()
   result2 = data2["message"]
-
-  return render(request, 'templates/index.html', {'result': result, 'result2': result2},)
+  match = re.search(r'breeds/([^/]+)/', result2)
+  breed_name = None
+  if match:
+    breed_name = match.group(1)
+    print(breed_name)
+  else:
+    print("No match found")
+  # Example 3
+  respon3 = requests.get('https://api.thecatapi.com/v1/images/search')
+  data3 = respon3.json()
+  result3 = data3[0]["url"]
+  return render(request, 'templates/index.html', {'result': result, 'result2': result2, 'breed_name': breed_name, 'result3': result3})
